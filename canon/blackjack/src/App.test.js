ac4816cdfde84af6e71f4de6057ced05b5263207
nik frank <nikbfrank@gmail.com>
2017-10-20 17:17:32 +0300

    enzyme testing in blackjack

--- canon/blackjack/src/App.test.js
+++ canon/blackjack/src/App.test.js
@@ -2,7 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { mount } from 'enzyme';
+
+import underground from 'react-underground';
+
+const UgApp = underground(App);
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<UgApp initialState={App.initialState}/>, div);
+});
+
+
+it('deals the hand from decays', ()=>{
+  // mount the component HOCd
+  // pass in an initial state of empty cards
+
+  // expect that the state will update via async decay
+  // expect that the state will stabilize at { cp.length: 1, p.length: 2 }
 });
