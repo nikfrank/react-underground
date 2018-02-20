2828b5fa1444cdc166499789c33d38322601bc51
nik frank <nikbfrank@gmail.com>
2018-02-20 20:02:28 +0200

    draw decays, lines... almost intelligible

--- src/index.js
+++ src/index.js
@@ -15,12 +15,14 @@ const sourceStamp = (id = guid())=>({
   timestamp: (new Date()).getTime(),
   source: id,
   id,
+  arity: 0,
 });
 
-const downstreamStamp = (a)=>({
+const downstreamStamp = (a)=> ({
   source: a.source,
   prev: a.id,
   id: guid(),
+  arity: a.arity + 1,
 });
 
 
@@ -88,7 +90,10 @@ export default P=> class nuP extends Component {
             }) )
 
           if ( effects.length )
-            this.props.onDecays( a, this.state, causedDecays, effects );
+            this.props.onDecays( a,
+                                 this.state,
+                                 causedDecays.map( d => d.name ),
+                                 effects );
           
           effects.forEach( this.dispatch );
         })
@@ -159,11 +164,12 @@ export default P=> class nuP extends Component {
 
 // uglogger
 const typedActions = (A, {
-  hook, trigger, reducer
+  hook, trigger, reducer, decays,
 } = A) => [
   hook ? { ...A, type: 'hook'} : undefined,
   trigger ? { ...A, type: 'trigger'} : undefined,
   reducer ? { ...A, type: 'reducer'} : undefined,
+  decays ? { ...A, type: 'decays'} : undefined,
 ].filter(i=>i);
 
 const timedAction = (sources = [], A) => ({
@@ -174,22 +180,26 @@ const timedAction = (sources = [], A) => ({
   ),
 });
 
-const CAfills = {
-  hook: 'red',
-  trigger: 'blue',
-  reducer: 'green',
+const CAstrokes = {
+  action: 'red',
+  trigger: 'orange',
+  reducer: 'blue',
+  hook: 'green',
+  decays: 'yellow',
 };
 
-const CAstrokes = {
-  hook: 'orange',
-  trigger: 'cyan',
-  reducer: 'yellow',
+const CAfills = {
+  action: '#f44',
+  trigger: '#fc7',
+  reducer: '#77f',
+  hook: '#4f4',
+  decays: '#ff4',
 };
 
 export class UgLogger extends Component {
 
   state = {
-    open: false,
+    open: true,
     actions: {},
     currentAction: '',
   };
@@ -201,7 +211,24 @@ export class UgLogger extends Component {
         .concat( typedActions( timedAction(state.actions[A.source], A) ) )
         .sort((a, b)=>  a.timestamp - b.timestamp),
     },
+
+    currentAction: state.currentAction ||
+                   (state.actions && Object.keys(state.actions)[0]),
   }) );
+
+  onDecays = (A, s, c, e)=>
+    this.setState(state => ({
+      ...state, actions: {
+        ...state.actions,
+        [A.source]: (state.actions[A.source] || [])
+          .concat( typedActions( timedAction(state.actions[A.source], {
+            source: A.source, prev: A.id, nexts: e.map(e=> e.id),
+            timestamp: A.timestamp + 1, arity: A.arity + 0.5,
+            decays: c,
+          }) ) )
+          .sort((a, b)=>  a.timestamp - b.timestamp),
+      },
+    }) )
 
   setCurrentAction = (actionId)=>{
     this.setState({ currentAction: actionId });
@@ -216,26 +243,29 @@ export class UgLogger extends Component {
   
   render(){
     const {
-      onAction = (...a)=> console.log('A', ...a),
-      onTrigger = (...a)=> console.log('T', ...a),
-      onDecays = (...a)=> console.log('Y', ...a),
-      onHook = (...a)=> console.log('H', ...a),
+      onAction = (...a)=> false && console.log('A', ...a),
+      onTrigger = (...a)=> false && console.log('T', ...a),
+      onDecays = this.onDecays,
+      onHook = (...a)=> false && console.log('H', ...a),
       Component,
       ...props,
     } = this.props;
 
     const currentAction = this.state.currentAction;
     const cActions = this.state.actions[currentAction] || [];
+    const cActionIndex = {};
+    cActions.forEach( a => (cActionIndex[a.id] = a) );
+console.log(cActions);
     
     return (
       <div>
         <Component
-          onDispatch={this.onDispatch}
-          onAction={onAction}
-          onTrigger={onTrigger}
-          onDecays={onDecays}
-          onHook={onHook}
-          {...props}
+            onDispatch={this.onDispatch}
+            onAction={onAction}
+            onTrigger={onTrigger}
+            onDecays={onDecays}
+            onHook={onHook}
+            {...props}
         />
         
         <div style={{
@@ -251,7 +281,7 @@ export class UgLogger extends Component {
           !this.state.open ? null : (
             <div style={{
               position: 'fixed', top: 0, right: 0,
-              height: '100vh', width: '18vw',
+              height: '100vh', width: '36vw',
               borderLeft: '1px solid black',
               backgroundColor: 'white',
               zIndex: 5,
@@ -269,17 +299,33 @@ export class UgLogger extends Component {
 
               {
                 !this.state.currentAction ? null : (
-                  <svg viewBox="0 0 100 1000">
+                  <svg viewBox="0 0 200 1000">
                     {
-                      cActions.map( ({ id, type, timediff, spacediff }, cai) => (
+                      cActions.map( ({
+                        id, type, timediff, spacediff, arity, prev, nexts,
+                      }, cai) => [
                         <circle key={id+''+cai} r={4}
-                                cx={25 + spacediff} cy={30 + (timediff/5)}
+                                cx={25 + spacediff} cy={30 + (arity * 20)}
                                 fill={CAfills[type]}
                                 stroke={CAstrokes[type]}
                                 strokeWidth={2}
                                 onClick={this.displayAction(id)}
-                        />
-                      ) )
+                        />,
+
+                        prev && (
+                          <line x1={25 + spacediff} y1={30 + (arity * 20)}
+                                x2={cActionIndex[prev].spacediff + 25}
+                                y2={cActionIndex[prev].arity * 20 + 30}
+                                strokeWidth='1' stroke='black' key={id+' p '+cai}/>
+                        ),
+
+                        ...(nexts||[]).map( next=> (
+                          <line x1={25 + spacediff} y1={30 + (arity * 20)}
+                                x2={cActionIndex[next].spacediff + 25}
+                                y2={cActionIndex[next].arity * 20 + 30}
+                                strokeWidth='1' stroke='green' key={id+'n '+cai+''+next}/>
+                        ) ),
+                      ] )
                     }
                   </svg>
                 )
